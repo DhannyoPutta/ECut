fa10568ce3fb50cc22b1ea5ae3c702ddf86f8e46
DhannyoPutta <dhannyo.p@gmail.com>
2024-11-30 20:59:46 +0700

    adding logo, linking jumbotron, modifying main page

--- src/ECut_frontend/src/component/navbar/Navbar.jsx
+++ src/ECut_frontend/src/component/navbar/Navbar.jsx
@@ -4,10 +4,7 @@ const NavigationBar = () => {
   return (
     <nav className='flex justify-around items-center fixed top-0 px-5 py-5 w-full box-border text-tertiary bg-white z-20'>
       <div className="flex">
-        <img src="./logo.png" alt="ECut" className='w-12 h-10' />
-        <div className='font-bold flex items-center pl-4 text-3xl'>
-          ECut
-        </div>
+        <img src="./ecut_logo.png" alt="ECut" className='w-29 h-10' />
       </div>
       <ul className="flex gap-6 text-2xl items-center font-semibold">
         <li><a href="explore">Explore</a></li>
