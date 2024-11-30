48ff894470b526705ec40ea5a8a1036f0dcc7e9d
karYa1 <melvindtk@gmail.com>
2024-11-30 12:47:52 +0700

    deleted uwanted file

--- HACK_BECAUSE_OF_ALLOW_JS/ECut_frontend/src/component/loading/page.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-const LoadingPage = () => {
-    return (<div className='w-screen h-screen bg-white flex justify-center items-center'>Loading...</div>);
-};
-export default LoadingPage;
